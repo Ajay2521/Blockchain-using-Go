afcd89b407a3d014ffadcd3658aa30e95ff4da0b
Ajay <ajay.m.200521@gmail.com>
2024-05-04 15:03:38 +0530

    Added Server

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"goblockchain/block"
-	"goblockchain/wallet"
+	"flag"
+	server "goblockchain/server"
 	"log"
 )
 
@@ -12,23 +11,8 @@ func init() {
 }
 
 func main() {
-	walletM := wallet.NewWallet()
-	walletA := wallet.NewWallet()
-	walletB := wallet.NewWallet()
-
-	// Wallet
-	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
-
-	// Blockchain
-	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
-	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0,
-		walletA.PublicKey(), t.GenerateSignature())
-	fmt.Println("Added? ", isAdded)
-
-	blockchain.Mining()
-	blockchain.PrintBlockChain()
-
-	fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
-	fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
-	fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletM.BlockchainAddress()))
+	port := flag.Uint("port", 5001, "TCP Port Number for Blockchain Server")
+	flag.Parse()
+	app := server.NewServer(uint16(*port))
+	app.Run()
 }
